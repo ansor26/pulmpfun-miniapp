3f29f19b18ef180b06d073ef0079806d0dcfb751
ansor26 <70452841+ansor26@users.noreply.github.com>
2025-08-17 17:11:15 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
+declare global { interface Window { Telegram: any } }
+
+const tg = (window as any)?.Telegram?.WebApp
+if (tg) {
+  tg.ready()
+  tg.expand()                   // растягиваем на всю высоту
+  tg.enableClosingConfirmation()
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
